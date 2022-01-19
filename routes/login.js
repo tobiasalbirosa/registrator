b349bd354da12eca7ee894cf5fcc4d1503e9d7eb
tobjj <tobiasalbirosa@gmail.com>
2022-01-19 19:15:12 -0300

    post

--- routes/login.js
+++ routes/login.js
@@ -2,9 +2,9 @@
 
 const express = require(`express`)
 const login = express.Router()
+const checkLogin = require(`../middlewares/checkLogin`)
 
 login.post(`/login`, (req,res,next) => {
-    const checkLogin = require(`../middlewares/checkLogin`).default
     checkLogin(req.body.email, req.body.password, req, res, next)
 })
 
