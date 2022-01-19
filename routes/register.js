491fb9800bc25fe0980c0b80388a0f5779efbc27
tobjj <tobiasalbirosa@gmail.com>
2022-01-19 19:15:29 -0300

    m

--- routes/register.js
+++ routes/register.js
@@ -2,10 +2,10 @@
 
 const express = require(`express`)
 const register = express.Router()
+const checkRegister = require(`../middlewares/checkRegister`)
 
 register.post(`/register`, (req, res, next) => {
     
-    const checkRegister = require(`../middlewares/checkRegister`)
     checkRegister(req.body.email, req.body.password, req.body.confirmpassword, req, res, next)
 
 })
